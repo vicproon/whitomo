a5a0782039c0c1d4d0491b2f0047806b5f3420ae
vicproon <vicproon@gmail.com>
2018-03-18 01:04:31 +0300

    barrier method implementation works and even shows better results than previous whitomo

---
+++ src/barrier_method_gogo.py
@@ -0,0 +1,138 @@
+# /usr/bin/python
+# coding=utf-8
+
+from __future__ import print_function, division
+import white_projection
+import white_rec
+import barrier_method
+import phantoms
+import scipy, scipy.stats
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+input_data_dict = phantoms.get_input('button_4_synth')
+
+energy_grid = input_data_dict['grid']
+gt_concentrations = input_data_dict['gt_concentrations']
+# tmp = gt_concentrations.copy()
+# gt_concentrations[0] = tmp[1]
+# gt_concentrations[1] = tmp[0]
+# gt_concentrations = tmp
+source = input_data_dict['source']
+pixel_size = input_data_dict['pixel_size']
+element_numbers = input_data_dict['element_numbers']
+element_absorptions = input_data_dict['element_absorptions']
+ph_size = gt_concentrations.shape[1:]
+
+# parameters of reconstruction
+n_angles = 360   # projection angles
+alpha = 0.7    # gradient step (aka relaxation aka learning rate)
+beta_reg = 1e-2   # regularization coefficiten [update = alpha * (BP + beta * reg)]
+
+# set up tomography operator
+wp = white_projection.WhiteProjection(source=source,
+    pixel_size=pixel_size, element_numbers=element_numbers,
+    element_absorptions=element_absorptions, ph_size=ph_size,
+    n_angles=n_angles, energy_grid=energy_grid)
+
+wp.calc_sinogram_with_gt(gt_concentrations)
+
+goal = (lambda x: wp.func(x), lambda x: wp.grad(x))
+
+# init arrays for the concentrations:
+
+conc_shape = (len(element_numbers), ph_size[0], ph_size[1])
+# concentrations = np.zeros(shape=conc_shape, dtype=np.float64)
+# init as truncated normal
+concentrations = scipy.stats.truncnorm(
+    a=-0.1, b=0.1, scale=0.1).rvs(size=conc_shape) 
+# concentrations += np.array(gt_concentrations)
+concentrations += 0.15
+
+# test if goal works
+def test_goal():
+    global concentrations
+    f = goal[0](concentrations)
+    g = goal[1](concentrations)
+
+    print('f size is', f.shape, 'g size is', g.shape)
+
+    print('making some iters...')
+    for i in range(100):
+        concentrations -= goal[1](concentrations)
+
+    plt.subplot(121)
+    plt.imshow(concentrations[0])
+    plt.subplot(122)
+    plt.imshow(concentrations[1])
+    plt.show()
+
+# test_goal()
+
+# ==============
+# initialize c1*c2 regularisation
+ir_beta = 0.05
+ineq_reg = (lambda x: ir_beta * np.abs(x[0] * x[1]), 
+            lambda x: ir_beta * white_rec.calc_uneq_reg_grad(x))
+
+# ==============
+# initialize C constraints
+
+# c >= 0, i.e. -c <= 0
+conc_non_negative = (lambda x: -x, lambda x: -np.ones_like(x))
+
+# c <= 1, i.e. c - 1 <= 0
+conc_less_than_one = (lambda x: x - 1, lambda x: np.ones_like(x))
+
+# ==============
+# run barrier method with C constraints only
+ans, opt_stats = barrier_method.barrier_method(concentrations,
+    goal,
+    reg_dict={'ineq_reg': ineq_reg},
+    ineq_dict={'conc_non_negative': conc_non_negative,
+               'conc_less_than_one': conc_less_than_one},
+    n_iter=100,
+    n_biter=10,
+    t0=0.1,
+    t_step=0.2,
+    beta_reg=1.0)
+
+plt.subplot(121)
+plt.imshow(ans[0])
+plt.subplot(122)
+plt.imshow(ans[1])
+plt.show()
+
+
+# ==============
+# stat plots
+x = np.arange(len(opt_stats))
+loss = np.array([stat[0].func for stat in opt_stats])
+grad_norm = np.array([np.linalg.norm(stat[0].grad) for stat in opt_stats])
+
+max_non_negative = np.array([(-np.exp(-stat[2]['conc_non_negative'].func)).max() for stat in opt_stats])
+grad_norm_non_neg = np.array([np.linalg.norm(stat[2]['conc_non_negative'].grad) for stat in opt_stats])
+
+max_less_than_one = np.array([(-np.exp(-stat[2]['conc_less_than_one'].func)).max() for stat in opt_stats])
+grad_norm_less_one = np.array([np.linalg.norm(stat[2]['conc_less_than_one'].grad) for stat in opt_stats])
+
+plt.subplot(221)
+plt.plot(x, loss)
+plt.title('goal loss')
+
+plt.subplot(222)
+plt.plot(x, grad_norm)
+plt.title('goal grad norm')
+
+plt.subplot(223)
+plt.plot(x, max_non_negative, x , max_less_than_one)
+plt.legend(['$c \geq 0$', '$c \leq 1$'])
+plt.title('max constraints values')
+
+plt.subplot(224)
+plt.plot(x, grad_norm_non_neg, x, grad_norm_less_one)
+plt.legend(['$c \geq 0$', '$c \leq 1$'])
+plt.title('grad norms for barrier functions')
+
+plt.show()
